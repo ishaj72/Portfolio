37133e5a2a898067cc1515f2962dae26d410d3c5
ishajain <isha30435@gmail.com>
2023-09-15 14:08:30 +0530

    readme file with changes

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-This is my portfolio.<br/>
-Created using HTML , CSS and JS.<br/>
-The contact me section will be updated later.<br/>
+# My Portfolio
+Showcasing my coding journey through projects.<br/>
+
+### Table of Contents
+- Home<br/>
+- About<br/>
+- Skills<br/>
+- Project<br/>
+- Contact<br/>
+
+### Technologies Used
+
+- HTML<br/>
+- CSS<br/>
+- JavaScript<br/>
+
+## Installation
+
+1. Clone this repository: <br/>
+   git clone https://github.com/ishaj72/Portfolio 
+<br/>
+
+2. Open the project folder:
+   cd Portfolio
+<br/>
+
+3. Screenshots:
+   ![Screenshot 1](screenshots/screenshot1.png)
+   ![Screenshot 2](screenshots/screenshot2.png)
+   ![Screenshot 3](screenshots/screenshot3.png)
+   ![Screenshot 4](screenshots/screenshot4.png)
--- index.js
+++ index.js
@@ -1,32 +1,54 @@
-let menuIcon = document.querySelector('.menu-icon');
-let navbar = document.querySelector('.navbar');
+// let menuIcon = document.querySelector('.menu-icon');
+// let navbar = document.querySelector('.navbar');
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-};
+// menuIcon.onclick = () => {
+//     menuIcon.classList.toggle('bx-x');
+//     navbar.classList.toggle('active');
+// };
 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop - 150;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
 
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav [href*="'+id+'"]').classList.add('active');
-            });
-        }
+//         if (top >= offset && top < offset + height) {
+//             navLinks.forEach(links => {
+//                 links.classList.remove('active');
+//                 document.querySelector('header nav [href*="'+id+'"]').classList.add('active');
+//             });
+//         }
+//     });
+
+//     let header = document.querySelector('header');
+//     header.classList.toggle('sticky', window.scrollY > 100);
+
+//     menuIcon.classList.remove('bx-x');
+//     navbar.classList.remove('active');
+// };
+
+document.addEventListener("DOMContentLoaded", function () {
+    const contactForm = document.getElementById("contact-form");
+
+    contactForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        // Get form input values
+        const fullName = document.querySelector('input[name="Full Name"]').value;
+        const emailAddress = document.querySelector('input[name="Email Address"]').value;
+        const mobileNumber = document.querySelector('input[name="Mobile Number"]').value;
+        const emailSubject = document.querySelector('input[name="Email Subject"]').value;
+        const message = document.querySelector('textarea[name="Your Message"]').value;
+
+        // Display an alert with form data
+        const formData = `Full Name: ${fullName}\nEmail Address: ${emailAddress}\nMobile Number: ${mobileNumber}\nEmail Subject: ${emailSubject}\nYour Message: ${message}`;
+        alert("Form submitted with the following data:\n" + formData);
+
+        // Reset form inputs
+        contactForm.reset();
     });
-
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 100);
-
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-};
+});
